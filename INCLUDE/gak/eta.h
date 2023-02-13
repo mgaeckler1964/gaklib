1963bfaabaaf65fac689851e8ffa27f6178eca91
Martin Gäckler <git@gaeckler.at>
2023-02-13 22:31:49 +0100

    updated header

--- INCLUDE/gak/eta.h
+++ INCLUDE/gak/eta.h
@@ -6,7 +6,7 @@
 		Address:		Hopfengasse 15, A-4020 Linz
 		Web:			http://www.gaeckler.at/
 
-		Copyright:		(c) 1988-2020 Martin Gäckler
+		Copyright:		(c) 1988-2023 Martin Gäckler
 
 		This program is free software: you can redistribute it and/or modify  
 		it under the terms of the GNU General Public License as published by  
@@ -15,7 +15,7 @@
 		You should have received a copy of the GNU General Public License 
 		along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-		THIS SOFTWARE IS PROVIDED BY Martin Gäckler, Germany, Munich ``AS IS''
+		THIS SOFTWARE IS PROVIDED BY Martin Gäckler, Linz, Austria ``AS IS''
 		AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
 		TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
 		PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR
