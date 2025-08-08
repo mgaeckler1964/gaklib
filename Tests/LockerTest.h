3e099a3849d8231a5bb9e9380d5bc7ee8976464d
Martin Gäckler <git@gaeckler.at>
2025-08-08 13:01:26 +0200

    more tests

--- Tests/LockerTest.h
+++ Tests/LockerTest.h
@@ -1,7 +1,7 @@
 /*
 		Project:		GAKLIB
-		Module:			LogFilkeTest,h
-		Description:	Tesst logging and profiling
+		Module:			LockerTest.h
+		Description:	Test the locker
 		Author:			Martin Gäckler
 		Address:		Hofmannsthalweg 14, A-4030 Linz
 		Web:			https://www.gaeckler.at/
@@ -38,7 +38,10 @@
 // ----- includes ------------------------------------------------------ //
 // --------------------------------------------------------------------- //
 
-#include <gak/thread.h>
+#include <iostream>
+#include <gak/unitTest.h>
+
+#include <gak/locker.h>
 
 // --------------------------------------------------------------------- //
 // ----- imported datas ------------------------------------------------ //
@@ -54,6 +57,9 @@
 #	pragma option -a4
 #	pragma option -pc
 #endif
+
+namespace gak
+{
 
 // --------------------------------------------------------------------- //
 // ----- constants ----------------------------------------------------- //
@@ -71,175 +77,62 @@
 // ----- class definitions --------------------------------------------- //
 // --------------------------------------------------------------------- //
 
-#include <iostream>
-#include <gak/unitTest.h>
-
-namespace gak
+static const unsigned long TIMEOUT = 1000;
+struct TestThread : public Thread
 {
+	bool	m_ok;
+	Locker	&m_locker;
 
-struct ThreadProfiler : public gak::Thread
-{
-	static const size_t s_loop_count;
-	size_t m_count;
-
-	gakLogging::LogLevel m_logLevel;
-
-
-	void ProfileThreadTest2();
-	void ProfileThreadTest();
-
-	ThreadProfiler(gakLogging::LogLevel ll) : Thread(false), m_count(0), m_logLevel(ll)
+	TestThread(Locker &locker) : Thread(false), m_locker(locker), m_ok(false)
 	{}
 
 	void ExecuteThread()
 	{
-		doEnterFunctionEx(gakLogging::llInfo, "ThreadProfiler::ExecuteThread");
-		ProfileThreadTest();
+		m_ok = m_locker.lock(TIMEOUT);
 	}
 };
 
-class LogfileTest : public UnitTest
+class LockerTest : public UnitTest
 {
-	virtual const char *GetClassName() const
+	virtual const char *GetClassName( void ) const
 	{
-		return "LogfileTest";
+		return "LockerTest";
 	}
-
-	static const size_t s_loop_count;
-	size_t count;
-
-	void ProfileTestNone2();
-	void ProfileTestNone();
-	void ProfileTestDetail2();
-	void ProfileTestDetail();
-	void ProfileTestInfo2();
-	void ProfileTestInfo();
-	void ProfileTest();
-
-	virtual void PerformTest()
+	virtual void PerformTest( void )
 	{
-		doEnterFunctionEx(gakLogging::llInfo, "LogfileTest::PerformTest");
+		TestScope scope( "PerformTest" );
 
-		doLogPosition();
-		doLogPositionEx(gakLogging::llInfo);
+		Locker	locker;
+		
+		UT_ASSERT_EQUAL( locker.getLockCount(), 0 );
 
-		bool value=true;
-		doLogValue(value );
-		doLogValueEx(gakLogging::llInfo, value );
+		locker.lock();
+		UT_ASSERT_EQUAL( locker.getLockCount(), 1 );
 
-		doLogMessage( "Hello world" );
-		doLogMessageEx( gakLogging::llInfo, "Hello world" );
+		locker.lock();
+		UT_ASSERT_EQUAL( locker.getLockCount(), 2 );
 
-#ifdef __WINDOWS__
-		doLogError( GetLastError() );
-#endif
+		locker.unlock();
+		UT_ASSERT_EQUAL( locker.getLockCount(), 1 );
 
-		ProfileTest();
+		locker.unlock();
+		UT_ASSERT_EQUAL( locker.getLockCount(), 0 );
+
+		{
+			LockGuard lg( locker );
+			UT_ASSERT_EQUAL( locker.getLockCount(), 1 );
+
+			TestThread	thread( locker );
+
+			StopWatch sw(true);
+			thread.StartThread();
+			thread.join();
+			UT_ASSERT_EQUAL( thread.m_ok, false );
+			UT_ASSERT_GREATEREQ( sw.getMillis(), clock_t(TIMEOUT) );
+		}
+		UT_ASSERT_EQUAL( locker.getLockCount(), 0 );
 	}
 };
-
-
-#ifdef NDEBUG
-const size_t LogfileTest::s_loop_count = 10000000;
-const size_t ThreadProfiler::s_loop_count = 10000000;
-#else
-const size_t LogfileTest::s_loop_count = 100000;
-const size_t ThreadProfiler::s_loop_count = 100000;
-#endif
-
-
-void ThreadProfiler::ProfileThreadTest2()
-{
-	doEnterFunctionEx(m_logLevel, "ThreadProfiler::ProfileThreadTest2");
-	m_count++;
-}
-
-void ThreadProfiler::ProfileThreadTest()
-{
-	doEnterFunctionEx(gakLogging::llInfo, "ThreadProfiler::ProfileThreadTest");
-	for( int i=0; i<s_loop_count; ++i )
-	{
-		ProfileThreadTest2();
-	}
-}
-
-void LogfileTest::ProfileTestNone2()
-{
-	count++;
-}
-
-void LogfileTest::ProfileTestNone()
-{
-	doEnterFunctionEx(gakLogging::llInfo, "LogfileTest::ProfileTestNone");
-	for( int i=0; i<s_loop_count; ++i )
-	{
-		ProfileTestNone2();
-	}
-}
-
-void LogfileTest::ProfileTestDetail2()
-{
-	doEnterFunctionEx(gakLogging::llDetail, "LogfileTest::ProfileTestDetail2");
-	count++;
-}
-
-void LogfileTest::ProfileTestDetail()
-{
-	doEnterFunctionEx(gakLogging::llInfo, "LogfileTest::ProfileTestDetail");
-	for( int i=0; i<s_loop_count; ++i )
-	{
-		ProfileTestDetail2();
-	}
-}
-
-void LogfileTest::ProfileTestInfo2()
-{
-	doEnterFunctionEx(gakLogging::llInfo, "LogfileTest::ProfileTestInfo2");
-	count++;
-}
-
-void LogfileTest::ProfileTestInfo()
-{
-	doEnterFunctionEx(gakLogging::llInfo, "LogfileTest::ProfileTestInfo");
-	for( int i=0; i<s_loop_count; ++i )
-	{
-		ProfileTestInfo2();
-	}
-}
-
-void LogfileTest::ProfileTest()
-{
-	doEnterFunctionEx(gakLogging::llInfo, "LogfileTest::ProfileTest");
-
-	{
-		doEnterFunctionEx(gakLogging::llInfo, "LogfileTest::ProfileTest::SingleThread");
-		count = 0;
-		ProfileTestInfo();
-		ProfileTestDetail();
-		ProfileTestNone();
-		UT_ASSERT_EQUAL( count, 3*s_loop_count);
-	}
-
-	{
-		doEnterFunctionEx(gakLogging::llInfo, "LogfileTest::ProfileTest::MultiThread");
-
-		ThreadProfiler	detailThread( gakLogging::llDetail );
-		ThreadProfiler	infoThread( gakLogging::llInfo );
-
-		infoThread.StartThread("infoThread");
-		detailThread.StartThread("detailThread");
-
-		detailThread.join();
-		infoThread.join();
-
-		UT_ASSERT_EQUAL( detailThread.m_count, ThreadProfiler::s_loop_count);
-		UT_ASSERT_EQUAL( infoThread.m_count, ThreadProfiler::s_loop_count);
-	}
-}
-
-static LogfileTest	myLogfileTest;
-
-}	//namespace gak
 
 // --------------------------------------------------------------------- //
 // ----- exported datas ------------------------------------------------ //
@@ -248,6 +141,8 @@ static LogfileTest	myLogfileTest;
 // --------------------------------------------------------------------- //
 // ----- module static data -------------------------------------------- //
 // --------------------------------------------------------------------- //
+
+static LockerTest myLockerTest;
 
 // --------------------------------------------------------------------- //
 // ----- class static data --------------------------------------------- //
@@ -292,6 +187,8 @@ static LogfileTest	myLogfileTest;
 // --------------------------------------------------------------------- //
 // ----- entry points -------------------------------------------------- //
 // --------------------------------------------------------------------- //
+
+}	// namespace gak
 
 #ifdef __BORLANDC__
 #	pragma option -RT.
