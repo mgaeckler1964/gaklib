01bbd1d38c43ba775d92b3abd1e097e734b16a27
Martin Gäckler <git@gaeckler.at>
2026-06-27 07:37:14 +0200

    Update gridRegistry.cpp

--- VCL_TOOLS/gridRegistry.cpp
+++ VCL_TOOLS/gridRegistry.cpp
@@ -3,10 +3,10 @@
 		Module:			gridRegistry.cpp
 		Description:
 		Author:			Martin Gäckler
-		Address:		Hopfengasse 15, A-4020 Linz
+		Address:		Hofmannsthalweg 14, A-4030 Linz
 		Web:			https://www.gaeckler.at/
 
-		Copyright:		(c) 1988-2021 Martin Gäckler
+		Copyright:		(c) 1988-2026 Martin Gäckler
 
 		This program is free software: you can redistribute it and/or modify  
 		it under the terms of the GNU General Public License as published by  
@@ -15,7 +15,7 @@
 		You should have received a copy of the GNU General Public License 
 		along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-		THIS SOFTWARE IS PROVIDED BY Martin Gäckler, Germany, Munich ``AS IS''
+		THIS SOFTWARE IS PROVIDED BY Martin Gäckler, Linz, Austria ``AS IS''
 		AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
 		TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
 		PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR
@@ -37,6 +37,7 @@
 // ----- includes ------------------------------------------------------ //
 // --------------------------------------------------------------------- //
 
+#include <memory>
 #include <vcl.h>
 #include <dbgrids.hpp>
 #include <registry.hpp>
@@ -145,7 +146,7 @@ void saveColumSettings( const TDBGrid *theGrid, const char *registryBase )
 	gridRegistry += theGrid->Owner->Name;
 	gridRegistry += theGrid->Name;
 
-	TRegistry	*reg = new TRegistry;
+	std::auto_ptr<TRegistry> reg(new TRegistry);
 
 	if( reg->OpenKey( gridRegistry, true ) )
 	{
@@ -170,8 +171,6 @@ void saveColumSettings( const TDBGrid *theGrid, const char *registryBase )
 
 		reg->CloseKey();
 	}
-
-	delete reg;
 }
 
 void restoreColumSettings( const TDBGrid *theGrid, const char *registryBase )
@@ -185,7 +184,7 @@ void restoreColumSettings( const TDBGrid *theGrid, const char *registryBase )
 	gridRegistry += theGrid->Owner->Name;
 	gridRegistry += theGrid->Name;
 
-	TRegistry	*reg = new TRegistry;
+	std::auto_ptr<TRegistry> reg(new TRegistry);
 
 	if( reg->OpenKey( gridRegistry, false ) )
 	{
@@ -220,8 +219,6 @@ void restoreColumSettings( const TDBGrid *theGrid, const char *registryBase )
 
 		reg->CloseKey();
 	}
-
-	delete reg;
 }
 
 }	// namespace vcl
