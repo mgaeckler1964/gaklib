f36edbf7701213ecaf61607b508e1c68ee760ea0
Martin Gäckler <git@gaeckler.at>
2026-07-11 15:33:55 +0200

    Add getNext

--- INCLUDE/gak/list.h
+++ INCLUDE/gak/list.h
@@ -3,10 +3,10 @@
 		Module:			List.H
 		Description:	Doubly linked list class
 		Author:			Martin Gäckler
-		Address:		Hopfengasse 15, A-4020 Linz
+		Address:		Hofmannsthalweg 14, A-4030 Linz
 		Web:			https://www.gaeckler.at/
 
-		Copyright:		(c) 1988-2021 Martin Gäckler
+		Copyright:		(c) 1988-2026 Martin Gäckler
 
 		This program is free software: you can redistribute it and/or modify  
 		it under the terms of the GNU General Public License as published by  
@@ -15,7 +15,7 @@
 		You should have received a copy of the GNU General Public License 
 		along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-		THIS SOFTWARE IS PROVIDED BY Martin Gäckler, Germany, Munich ``AS IS''
+		THIS SOFTWARE IS PROVIDED BY Martin Gäckler, Linz, Austria ``AS IS''
 		AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
 		TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
 		PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR
@@ -423,6 +423,17 @@ class ListContainer : public Container
 		buff->setPosition( m_first, 0 );
 		return m_first;
 	}
+
+	/**
+		@brief returns the next entry 
+		@param [out] buff a ListCursor
+		@return the element NULL if not found
+	*/
+	ListEntry *getNext( ListCursor *buff )
+	{
+		return buff->getNext();
+	}
+
 	/**
 		@brief returns the last entry 
 		@param [out] buff a new ListCursor
