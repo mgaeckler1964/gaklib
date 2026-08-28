6b37a7abb2edaa962fa2ea191e8f388eb8753124
Martin Gäckler <git@gaeckler.at>
2026-08-29 01:28:36 +0200

    Update diff.cpp
    better diff, show line numbers and better checksum

--- CTOOLS/diff.cpp
+++ CTOOLS/diff.cpp
@@ -41,6 +41,9 @@
 #include <gak/io.h>
 #include <gak/string.h>
 
+#include <gak/stdlib.h>
+#include <gak/fmtNumber.h>
+
 // --------------------------------------------------------------------- //
 // ----- module switches ----------------------------------------------- //
 // --------------------------------------------------------------------- //
@@ -56,7 +59,7 @@ struct LineInfo
 {
 	size_t	lineNo;
 	size_t	comparedLine;
-	long	chksum;
+	uint32	chksum;
 	size_t	chksumCnt;
 	off_t	position;
 };
@@ -116,180 +119,232 @@ static int chksumCountSort( const void *ptr1, const void *ptr2 )
 }
 
 /*
-	compare functions
+	helper functions
 */
-static void checkStart( LineInfo *info1, LineInfo *info2 )
+/// connect two lines
+static void connect( LineInfo *line1, LineInfo *line2 )
 {
-	size_t	idx;
+	assert(line1->chksum == line2->chksum);
+	assert(line1->comparedLine == no_line);
+	assert(line2->comparedLine == no_line);
 
-	idx = 0;
-	while( 1 )
-	{
-		if( info1[idx].lineNo == no_line )
-/*v*/		break;
-		if( info2[idx].lineNo == no_line )
-/*v*/		break;
-		if( info1[idx].chksum == info2[idx].chksum )
-		{
-			info1[idx].comparedLine = idx;
-			info2[idx].comparedLine = idx;
-		}
-		else
-/*v*/		break;
-		idx++;
-	}
+	line1->comparedLine = line2->lineNo;
+	line2->comparedLine = line1->lineNo;
 }
 
-static void checkEnd( LineInfo *info1, LineInfo *info2 )
+static size_t findMatchingLine( LineInfo *curLine1, LineInfo *info2 )
 {
-	size_t	idx1, idx2;
+	doEnterFunctionEx( gakLogging::llInfo, "findMatchingLine" );
 
-	for( idx1 = 0; info1[idx1].lineNo != no_line; idx1++ )
-		;
-	for( idx2 = 0; info2[idx2].lineNo != no_line; idx2++ )
-		;
-
-	while( 1 )
+	/*
+		find the last line in file 2 that matches before the actual line in file 1
+			and
+		find the first line in file 2 that matches after the actual line in file 1
+	*/
+	size_t	lowerBound = 0;
+	size_t	upperBound = std::numeric_limits<std::size_t>::max();
+	size_t idx2=0;
+	LineInfo *curLine2 = info2;
+	for(; curLine2->lineNo != no_line; ++idx2, ++curLine2 )
 	{
-		idx1--;
-		idx2--;
-
-		if( (int)idx1 == -1 )
-/*v*/		break;
-		if( (int)idx2 == -1 )
-/*v*/		break;
-		if( info1[idx1].chksum == info2[idx2].chksum )
+		if( curLine2->comparedLine != no_line )				// it is a match
 		{
-			info1[idx1].comparedLine = idx2;
-			info2[idx2].comparedLine = idx1;
+			if(  curLine2->lineNo > lowerBound					// it is behind the last match
+			&&  curLine2->comparedLine < curLine1->lineNo )		// it matches before the current
+				lowerBound = curLine2->lineNo;
+
+			if(  curLine2->lineNo < upperBound					// it is behind the last match
+			&&  curLine2->comparedLine > curLine1->lineNo )		// it matches after the current
+				upperBound = curLine2->lineNo;
 		}
-		else
-/*v*/		break;
-	}
-}
-
-static size_t findMatchingLine( LineInfo *info1, size_t idx1, LineInfo *info2 )
-{
-	size_t	idx2;
-	size_t	maxLine;
-
-	maxLine = 0;
-	for( idx2=0; info2[idx2].lineNo != no_line; idx2++ )
-	{
-		if( info2[idx2].comparedLine != no_line
-		&&  info2[idx2].lineNo > maxLine
-		&&  info2[idx2].comparedLine < info1[idx1].lineNo )
-			maxLine = info2[idx2].lineNo;
 	}
 
-	for( idx2=0; info2[idx2].lineNo != no_line; idx2++ )
+	/*
+		the first line in file 2 between lower and upper bound that matches the actual line in file 1
+	*/
+	idx2=0;
+	curLine2 = info2;
+	for(; curLine2->lineNo != no_line; ++idx2, ++curLine2 )
 	{
-		if( info2[idx2].comparedLine == no_line
-		&&  info2[idx2].lineNo >= maxLine
-		&&  info2[idx2].chksum == info1[idx1].chksum )
+		if( curLine2->comparedLine == no_line
+		&&  curLine2->lineNo > lowerBound
+		&&  curLine2->lineNo < upperBound
+		&&  curLine2->chksum == curLine1->chksum )
 /*@*/		return idx2;
 	}
 
 	return no_line;
 }
 
-static void checkCount( LineInfo *info1, LineInfo *info2 )
-{
-	size_t	idx1, idx2;
-
-	for( idx1 = 0; info1[idx1].lineNo != no_line; idx1++ )
-	{
-		if( info1[idx1].comparedLine == no_line )
-		{
-			idx2 = findMatchingLine( info1, idx1, info2 );
-			if( idx2 != no_line
-			&& info1[idx1].chksumCnt == info2[idx2].chksumCnt )
-			{
-				info2[idx2].comparedLine = info1[idx1].lineNo;
-				info1[idx1].comparedLine = info2[idx2].lineNo;
-			}
-		}
-	}
-}
-
-static void checkFiles( LineInfo *info1, LineInfo *info2 )
-{
-	size_t	idx1, idx2;
-
-	for( idx1=0; info1[idx1].lineNo != no_line; idx1++ )
-	{
-		if( info1[idx1].comparedLine == no_line )
-		{
-			idx2 = findMatchingLine( info1, idx1, info2 );
-			if( idx2 != no_line )
-			{
-				info2[idx2].comparedLine = info1[idx1].lineNo;
-				info1[idx1].comparedLine = info2[idx2].lineNo;
-			}
-		}
-	}
-}
-
+/*
+	count lines with the same checkum (the lines are sorted by checksum)
+*/
 static void countChksum( LineInfo *info )
 {
-	size_t	idx1 = 0, idx2 = 0, count = 0;
-	long	chksum = -1;
+	doEnterFunctionEx( gakLogging::llInfo, "countChksum" );
+	size_t	count = 0;
+	uint32	chksum = info->chksum;
 
-	while( info[idx2].lineNo != no_line )
+	const LineInfo *curLine1 = info;	// used to count
+	LineInfo *curLine2 = info;			// store the count
+	while( curLine2->lineNo != no_line )
 	{
-		if( info[idx1].lineNo != no_line && info[idx1].chksum == chksum )
+		if( curLine1->lineNo != no_line && curLine1->chksum == chksum )
 		{
-			idx1++;
+			curLine1++;
 			count++;
 		}
 		else
 		{
-			for( ;info[idx2].lineNo != no_line && info[idx2].chksum == chksum; idx2++ )
-				info[idx2].chksumCnt = count;
+			for( ;curLine2->lineNo != no_line && curLine2->chksum == chksum; ++curLine2 )
+				curLine2->chksumCnt = count;
 
 			count = 0;
-			chksum = info[idx2].chksum;
+			chksum = curLine2->chksum;
 		}
 	}
 }
 
 /*
-	read files, create diff files
+	compare functions
 */
+/**
+	First phase: compare the lines at the begining of the files
+	untile the lines differ
+*/
+static void checkStart( LineInfo *info1, LineInfo *info2 )
+{
+	doEnterFunctionEx( gakLogging::llInfo, "checkStart" );
 
+	LineInfo	*curLine1 = info1;
+	LineInfo	*curLine2 = info2;
+	while( 1 )
+	{
+		if( curLine1->lineNo == no_line )
+/*v*/		break;
+		if( curLine2->lineNo == no_line )
+/*v*/		break;
+		if( curLine1->chksum == curLine2->chksum )
+		{
+			connect(curLine1, curLine2);
+		}
+		else
+/*v*/		break;
+
+		++curLine1;
+		++curLine2;
+	}
+}
+
+/*
+	sceond phase: compare the lines at end of the files
+	untile the lines differ
+*/
+static void checkEnd( LineInfo *info1, size_t nLines1, LineInfo *info2, size_t nLines2 )
+{
+	doEnterFunctionEx( gakLogging::llInfo, "checkEnd" );
+
+	size_t	idx1 = nLines1,
+			idx2 = nLines2;
+	LineInfo *curLine1 = info1+idx1;
+	LineInfo *curLine2 = info2+idx2;
+	while( 1 )
+	{
+		if( --idx1 == no_line )
+/*v*/		break;
+		if( --idx2 == no_line )
+/*v*/		break;
+
+		--curLine1;
+		--curLine2;
+
+		if( curLine1->chksum == curLine2->chksum )
+		{
+			connect( curLine1, curLine2 );
+		}
+		else
+/*v*/		break;
+	}
+}
+
+/**
+	third phase: compare the lines where the count did not change
+*/
+static void checkCount( LineInfo *info1, LineInfo *info2 )
+{
+	doEnterFunctionEx( gakLogging::llInfo, "checkCount" );
+	for( LineInfo *curLine1 = info1; curLine1->lineNo != no_line; ++curLine1 )
+	{
+		if( curLine1->comparedLine == no_line )
+		{
+			size_t	idx2 = findMatchingLine( curLine1, info2 );
+			if( idx2 != no_line )
+			{
+				LineInfo *curLine2 = info2 + idx2;
+				if( curLine1->chksumCnt == curLine2->chksumCnt )
+				{
+					connect(curLine1, curLine2);
+				}
+			}
+		}
+	}
+}
+
+/**
+	forth phase: try to connect all lines not yet connected
+*/
+static void checkFiles( LineInfo *info1, LineInfo *info2 )
+{
+	doEnterFunctionEx( gakLogging::llInfo, "checkFiles" );
+
+	for( LineInfo *curLine1 = info1; curLine1->lineNo != no_line; ++curLine1 )
+	{
+		if( curLine1->comparedLine == no_line )
+		{
+			size_t		idx2 = findMatchingLine( curLine1, info2 );
+			if( idx2 != no_line )
+			{
+				connect( curLine1, info2 + idx2 );
+			}
+		}
+	}
+}
+
+/*
+	read files
+*/
 static LineInfo *readFile( const STRING &fileName, size_t *nLines )
 {
-	bool		ignoreSpaces = true;
-	bool		space = false;
-	RFILE		fp;
-	LineInfo	*fileInfo;
-	size_t		lines;
-	size_t		maxLines;
-	STRING		lineBuffer;
-	size_t		idx, lastChar;
+	doEnterFunctionEx( gakLogging::llInfo, "readFile" );
+	bool				ignoreSpaces = true;
+	bool				space = false;
+	RFILE				fp;
+	Buffer<LineInfo>	fileInfo;
+	size_t				lines = 0;
+	size_t				maxLines = 0;
+	STRING				lineBuffer;
+	size_t				idx, lastChar;
 
-	fileInfo = NULL;
-	lines = 0;
-	maxLines = 0;
-	
 	fp.open( fileName );
 	while( true )
 	{
 		if( lines >= maxLines )
 		{
 			maxLines += 32;
-			fileInfo = (LineInfo*)realloc( fileInfo, maxLines * sizeof( *fileInfo ) );
+			fileInfo.resize( maxLines * sizeof( LineInfo ) );
 			if( !fileInfo )
 /*v*/			break;
 		}
-		fileInfo[lines].chksum = 0;
-		fileInfo[lines].lineNo = lines;
-		fileInfo[lines].comparedLine = size_t(-1);
-		fileInfo[lines].position = fp.getpos();
+		LineInfo	&curLine = fileInfo[lines];
+		curLine.chksum = 0;
+		curLine.lineNo = lines;
+
+		curLine.comparedLine = no_line;
+		curLine.position = fp.getpos();
 		lineBuffer = fp.gets();
 		if( fp.isEOF() )
 		{
-			fileInfo[lines].lineNo = no_line;
+			curLine.lineNo = no_line;
 /*v*/		break;
 		}
 		if( ignoreSpaces )
@@ -316,12 +371,13 @@ static LineInfo *readFile( const STRING &fileName, size_t *nLines )
 			if( ignoreSpaces && isspace(static_cast<unsigned char>(lineBuffer[idx])) )
 			{
 				if( !space )
-					fileInfo[lines].chksum++;
+					curLine.chksum++;
 				space = true;
 			}
 			else
 			{
-				fileInfo[lines].chksum += long(lineBuffer[idx]) * long(idx);
+				uint8 left = (uint8(idx&3)<<3);
+				curLine.chksum += uint32(lineBuffer[idx]) << left;
 				space = false;
 			}
 		}
@@ -330,67 +386,99 @@ static LineInfo *readFile( const STRING &fileName, size_t *nLines )
 	fp.close();
 
 	*nLines = lines +1;
-	return fileInfo;
+	return fileInfo.release();
 }
 
+/*
+	construct the difference information
+*/
 static STRING buildDiffFile(
 	const char *file1, const char *file2,
 	LineInfo *info1, LineInfo *info2
 )
 {
+	doEnterFunctionEx( gakLogging::llInfo, "buildDiffFile" );
 	STRING	diff;
 	RFILE	fp1, fp2;
-	size_t	idx1, idx2;
 	STRING	lineBuff;
 
 	// open the files
 	fp1.open( file1 );
 	fp2.open( file2 );
 
-	idx1 = 0;
-	idx2 = 0;
+	size_t idx1 = 0;
+	size_t idx2 = 0;
 
 	while( 1 )
 	{
-		if( info1[idx1].lineNo != no_line
-		&&  info1[idx1].comparedLine == no_line )
+		const LineInfo	&curLine1 = info1[idx1];
+		const LineInfo	&curLine2 = info2[idx2];
+		if( curLine1.lineNo == no_line && curLine2.lineNo == no_line )
+			break;
+
+		if( curLine1.lineNo != no_line
+		&&  curLine1.comparedLine == no_line )
 		{
-			fp1.setpos( info1[idx1].position );
+			fp1.setpos( curLine1.position );
 			lineBuff = fp1.gets();
 
-			diff += "-<";
+			diff += formatNumber( curLine1.lineNo+1, 5 );
+
+			diff += "       -<";
 			diff += lineBuff;
 			diff += '\n';
 			idx1++;
 		}
-		else if( info2[idx2].lineNo != no_line
-				&&  info2[idx2].comparedLine == no_line )
+		else if( curLine2.lineNo != no_line
+				&&  curLine2.comparedLine == no_line )
 		{
-			fp2.setpos( info2[idx2].position );
+			fp2.setpos( curLine2.position );
 			lineBuff = fp2.gets();
 
-			diff += "+>";
+			diff += "      ";
+			diff += formatNumber( curLine2.lineNo+1, 5 );
+			diff += " +>";
 			diff += lineBuff;
 			diff += '\n';
 			idx2++;
 		}
 		else
 		{
-			if( info1[idx1].lineNo != no_line )
+			if( curLine1.lineNo != no_line )
 			{
-				fp1.setpos( info1[idx1].position );
+				fp1.setpos( curLine1.position );
 				lineBuff = fp1.gets();
 
-				diff += "  ";
+				diff += formatNumber( curLine1.lineNo+1, 5 );
+				diff += ' ';
+				diff += formatNumber( curLine2.lineNo+1, 5 );
+				diff += ' ';
+#if 0			// for better debugging
+				diff += formatNumber( curLine1.comparedLine+1, 5 );
+				diff += ' ';
+				diff += formatNumber( curLine2.comparedLine+1, 5 );
+				diff += ' ';
+				diff += formatNumber( curLine1.chksum() );
+				diff += ' ';
+				diff += formatNumber( curLine2.chksum() );
+				diff += ' ';
+
+				if(curLine1.lineNo != curLine2.comparedLine || curLine2.lineNo != curLine1.comparedLine )
+					diff += "=========================================";
+#else			// for testing
+				assert(curLine1.lineNo == curLine2.comparedLine);
+				assert(curLine2.lineNo == curLine1.comparedLine);
+				assert(curLine1.chksum == curLine2.chksum);
+#endif
+				diff += "   ";
+
 				diff += lineBuff;
 				diff += '\n';
 				idx1++;
 			}
-			if( info2[idx2].lineNo != no_line )
+			if( curLine2.lineNo != no_line )
 				idx2++;
 		}
-		if( info1[idx1].lineNo == no_line && info2[idx2].lineNo == no_line )
-			break;
 	}
 
 	return diff;
@@ -402,21 +490,22 @@ static STRING buildDiffFile(
 
 const STRING &diff( const STRING &file1, const STRING &file2 )
 {
+	doEnterFunctionEx( gakLogging::llInfo, "diff" );
+
 	static STRING	diffInfo;
-	LineInfo		*info1, *info2;
 	size_t			nLines1, nLines2;
 
-	info1 = readFile( file1, &nLines1 );
+	Buffer<LineInfo> info1( true, readFile( file1, &nLines1 ) );
 	if( info1 )
 	{
-		info2 = readFile( file2, &nLines2 );
+		Buffer<LineInfo> info2( true, readFile( file2, &nLines2 ) );
 		if( info2 )
 		{
 			// check from beginig of file
 			checkStart( info1, info2 );
 
 			// check from end of file
-			checkEnd( info1, info2 );
+			checkEnd( info1, nLines1, info2, nLines2 );
 
 			qsort( info1, nLines1, sizeof( LineInfo ), chksumSort );
 			qsort( info2, nLines2, sizeof( LineInfo ), chksumSort );
@@ -435,10 +524,7 @@ const STRING &diff( const STRING &file1, const STRING &file2 )
 			qsort( info2, nLines2, sizeof( LineInfo ), lineSort );
 
 			diffInfo = buildDiffFile( file1, file2, info1, info2 );
-
-			free( info2 );
 		}
-		free( info1 );
 	}
 
 	return diffInfo;
